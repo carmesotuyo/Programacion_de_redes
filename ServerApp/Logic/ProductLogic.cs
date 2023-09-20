a8fb46813835d537c51869c11299d3b46d6821fb
carmesotuyo <carmela.sotuyo@gmail.com>
2023-09-19 21:44:27 -0300

    agrego logica para publicar productos

---
+++ ServerApp/Logic/ProductLogic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using ServerApp.Domain;
+using ServerApp.Database;
+
+namespace ServerApp.Logic
+{
+	public class ProductLogic
+	{
+		private readonly SingletonDB _database;
+
+		public ProductLogic()
+		{
+			_database = SingletonDB.GetInstance();
+		}
+
+		public Producto publicarProducto(Producto producto, Usuario usuario)
+		{
+			validarProductoRepetido(producto);
+			_database.agregarProducto(producto);
+			usuario.agregarProductoAPublicados(producto);
+			return producto; // podria traer el producto de la db confirmando que se guardó
+		}
+
+		private void validarProductoRepetido(Producto producto)
+		{
+			if (_database.existeProducto(producto))
+			{
+				throw new Exception("El producto ya existe publicado en el Marketplace, te ganaron de mano :(");
+			} else if (_database.existeImagen(producto.Imagen))
+			{
+				throw new Exception("Una imagen con ese nombre ya existe, probá cambiándolo :)");
+			}
+		}
+
+    }
+}
+
