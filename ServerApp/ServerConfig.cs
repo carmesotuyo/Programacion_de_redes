40f46f5060d6e09a7bfb0382a75c527429b63ad9
carmesotuyo <carmela.sotuyo@gmail.com>
2023-10-17 19:16:17 -0300

    fix bug cuelgue

--- ServerApp/ServerConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ServerApp
-{
-    public static class ServerConfig
-    {
-        public static string serverIPconfigkey = "ServerIpAddress";
-        public static string serverPortconfigkey = "ServerPort";
-        public static string imagePathconfigkey = "ImagePath";
-    }
-}
