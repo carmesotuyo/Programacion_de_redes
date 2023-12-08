d23d3cacb9dc6162e91bd9f5a0217700111088c9
carmesotuyo <carmela.sotuyo@gmail.com>
2023-12-08 12:47:17 -0300

    creo proyecto MainServer y Correo

---
+++ Correo/Program.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Net.Client;
+
+namespace Correo;
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        using var channel = GrpcChannel.ForAddress("http://localhost:5239");
+        var client = new Greeter.GreeterClient(channel);
+        var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+        Console.WriteLine("Greeting: " + reply.Message);
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+
+}
