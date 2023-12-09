a2c49be196d3142b38ba344bfa56d2b500a4a41e
NachoMalamud <ignacio.malamud@gmail.com>
2023-12-09 13:56:03 -0300

    Carpeta de services

--- Correo/Program.cs
+++ Correo/Program.cs
@@ -1,16 +1,23 @@
 ﻿using Grpc.Net.Client;
+using System;
 
-namespace Correo;
-class Program
+namespace Correo
 {
-    static async Task Main(string[] args)
+    class Program
     {
-        using var channel = GrpcChannel.ForAddress("http://localhost:5239");
-        var client = new Greeter.GreeterClient(channel);
-        var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
-        Console.WriteLine("Greeting: " + reply.Message);
-        Console.WriteLine("Press any key to exit...");
-        Console.ReadKey();
-    }
+        static void Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("http://localhost:5239");
+            var client = new Correo.CorreoClient(channel);
 
+            // Aquí deberías tener la lógica para obtener el destinatario y el mensaje
+            var destinatario = "usuario@example.com";
+            var mensaje = "Este es el cuerpo del correo...";
+
+            // Llama al método EnviarCorreo del servicio de correo
+            var response = client.EnviarCorreo(new EnviarCorreoRequest { Destinatario = destinatario, Mensaje = mensaje });
+
+            Console.WriteLine($"Respuesta del servidor: {response.Resultado}");
+        }
+    }
 }
