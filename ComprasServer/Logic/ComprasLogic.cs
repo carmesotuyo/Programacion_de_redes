882c4babec8705da6d4873f663124932bd65db19
Fernando Spillere <f.spillere269@gmail.com>
2023-12-09 22:05:56 -0300

    se agrega logica de compras

---
+++ ComprasServer/Logic/ComprasLogic.cs
@@ -0,0 +1,45 @@
+﻿using ComprasServer.Database;
+
+namespace ComprasServer.Logic
+{
+    public class ComprasLogic
+    {
+        private readonly SingletonDB _database;
+
+        public ComprasLogic()
+        {
+            _database = SingletonDB.GetInstance();
+        }
+
+        public Compra agregarCompra(Compra compra)
+        {
+            _database.agregarCompra(compra);
+            return compra;
+        }
+
+        public List<Compra> darListadoCompras()
+        {
+            return _database.darListaCompras();
+        }
+
+        public List<Compra> BuscarComprasPorProducto(string nombreProducto)
+        {
+            return _database.BuscarComprasPorProducto(nombreProducto);
+        }
+
+        public List<Compra> BuscarComprasPorUsuario(string usuario)
+        {
+            return _database.BuscarComprasPorUsuario(usuario);
+        }
+
+        public List<Compra> BuscarComprasPorFecha(DateTime fecha)
+        {
+            return _database.BuscarComprasPorFecha(fecha);
+        }
+
+        public List<Compra> BuscarComprasPorPrecio(float precio)
+        {
+            return _database.BuscarComprasPorPrecio(precio);
+        }
+    }
+}
---
+++ ComprasServer/Service/MQService.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Connections;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+
+namespace ComprasServer.Service
+{
+    public class MQService
+    {
+        public MQService() {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "Compras", type: ExchangeType.Fanout);
+
+                var queueName = channel.QueueDeclare().QueueName; //Declaro una queue por defecto
+
+                channel.QueueBind(queue: queueName,
+                                  exchange: "Compras",
+                                  routingKey: "");
+
+                Console.WriteLine(" [*] Esperando por Compras.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += async (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Compra compra = JsonSerializer.Deserialize<Compra>(message);
+
+
+                    //=========== Acá hago algo con el mensaje que llega ===========
+                    
+                    //Console.WriteLine(" [x] Enviando correo a " + compra.Usuario + " por comprar producto " + compra.NombreProducto);
+                    //await Task.Delay(5000);
+                    
+                    //==============================================================
+                };
+                channel.BasicConsume(queue: queueName,
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
